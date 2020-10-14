9ca20fe3c15c0716a9c134165e9103a36bba8cad
Abdullahalbakhit <70815008+Abdullahalbakhit@users.noreply.github.com>
2020-10-14 15:12:37 +0300

    البدايه

---
+++ m6b5k/m6b5k/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  m6b5k
+//
+//  Created by Abdullah Albakeet on 10/14/20.
+//  Copyright © 2020 Abdullah Albakeet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
